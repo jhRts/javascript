9d46490ffdee5dcd3c9c995cdefb73118032cecd
jhRts <jh@rts.dk>
2013-11-22 18:26:46 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+﻿/*
+
+::::::::: ::::::::::: ::::::::  
+:+:    :+:    :+:    :+:    :+: 
++:+    +:+    +:+    +:+        
++#++:++#:     +#+    +#++:++#++ 
++#+    +#+    +#+           +#+ 
+#+#    #+#    #+#    #+#    #+# 
+###    ###    ###     ########  
+
+*/
+
+
+var items = $("section");
+var ul = $('.bs-docs-sidenav');
+$(items).each(function () {
+    var item = $(this);
+    ul.append('<li><a href="#' + item.attr("id") + '"><i class="icon-chevron-right"></i>' + $("h1", item).text() + " " + $(".page-header .lead", item).text() + '</a></li>');
+});
+$("li:first", ul).addClass("active");
+
+
